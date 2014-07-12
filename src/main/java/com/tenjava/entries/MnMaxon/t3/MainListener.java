c2c14071c5772707f6811b920fc280dcabf3c501
MnMaxon <masontg777@aol.com>
2014-07-12 10:17:03 -0400

    Added Listener and set up main class

---
+++ src/main/java/com/tenjava/entries/MnMaxon/t3/MainListener.java
@@ -0,0 +1,6 @@
+package com.tenjava.entries.MnMaxon.t3;
+
+import org.bukkit.event.Listener;
+
+public class MainListener implements Listener {
+}
--- src/main/java/com/tenjava/entries/MnMaxon/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/MnMaxon/t3/TenJava.java
@@ -1,6 +1,19 @@
 package com.tenjava.entries.MnMaxon.t3;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TenJava extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new MainListener(), this);
+	}
+	@Override
+	public void onDisable() {
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		return false;
+	}
 }
