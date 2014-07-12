cdef65b700bb08a796a5a4eb09b6932a010a9c16
MnMaxon <masontg777@aol.com>
2014-07-12 11:51:52 -0400

    Finished selecting where villages appear

--- src/main/java/com/tenjava/entries/MnMaxon/t3/MainListener.java
+++ src/main/java/com/tenjava/entries/MnMaxon/t3/MainListener.java
@@ -23,7 +23,12 @@ public class MainListener implements Listener {
 					&& Math.rint(Math.random() * 100.0 / cfg.getDouble("Viallages.Percent of chunks")) == 0
 					&& cfg.getInt("Viallages.Max Chunks") > 0) {
 				villageChunks.add(e.getChunk());
-				e.getChunk();
+				int size = cfg.getInt("Viallages.Max Chunks") / 2;
+				size = (int) (size + Math.rint((cfg.getInt("Viallages.Max Chunks") - size) * Math.random()));
+				while (villageChunks.size() < size)
+					villageChunks = getSurroundingChunks(
+							villageChunks.get((int) (Math.random() * (villageChunks.size() - 1))), villageChunks, size);
+				new Village(villageChunks);
 			}
 			if (villageChunks.isEmpty() && cfg.getDouble("Dungeons.Percent of chunks") > 0.0
 					&& Math.rint(Math.random() * 100.0 / cfg.getDouble("Dungeons.Percent of chunks")) == 0
@@ -35,9 +40,29 @@ public class MainListener implements Listener {
 	private static ArrayList<Chunk> getSurroundingChunks(Chunk chunk, ArrayList<Chunk> selectedChunks, int size) {
 		YamlConfiguration cfg = Config.load("Config.yml");
 		if (!selectedChunks.contains(chunk.getWorld().getChunkAt(chunk.getX() + 1, chunk.getZ()))
-				&& Math.rint(Math.random()) == 1 && selectedChunks.size() <= size) {
+				&& Math.rint(Math.random()) == 1 && selectedChunks.size() <= size)
 			selectedChunks.add(chunk.getWorld().getChunkAt(chunk.getX() + 1, chunk.getZ()));
-		}
+		if (!selectedChunks.contains(chunk.getWorld().getChunkAt(chunk.getX() + 1, chunk.getZ() - 1))
+				&& Math.rint(Math.random()) == 1 && selectedChunks.size() <= size)
+			selectedChunks.add(chunk.getWorld().getChunkAt(chunk.getX() + 1, chunk.getZ() - 1));
+		if (!selectedChunks.contains(chunk.getWorld().getChunkAt(chunk.getX() + 1, chunk.getZ()))
+				&& Math.rint(Math.random()) == 1 && selectedChunks.size() <= size)
+			selectedChunks.add(chunk.getWorld().getChunkAt(chunk.getX() + 1, chunk.getZ() + 1));
+		if (!selectedChunks.contains(chunk.getWorld().getChunkAt(chunk.getX(), chunk.getZ() + 1))
+				&& Math.rint(Math.random()) == 1 && selectedChunks.size() <= size)
+			selectedChunks.add(chunk.getWorld().getChunkAt(chunk.getX(), chunk.getZ() + 1));
+		if (!selectedChunks.contains(chunk.getWorld().getChunkAt(chunk.getX(), chunk.getZ() - 1))
+				&& Math.rint(Math.random()) == 1 && selectedChunks.size() <= size)
+			selectedChunks.add(chunk.getWorld().getChunkAt(chunk.getX(), chunk.getZ() - 1));
+		if (!selectedChunks.contains(chunk.getWorld().getChunkAt(chunk.getX() - 1, chunk.getZ()))
+				&& Math.rint(Math.random()) == 1 && selectedChunks.size() <= size)
+			selectedChunks.add(chunk.getWorld().getChunkAt(chunk.getX() - 1, chunk.getZ()));
+		if (!selectedChunks.contains(chunk.getWorld().getChunkAt(chunk.getX() - 1, chunk.getZ() - 1))
+				&& Math.rint(Math.random()) == 1 && selectedChunks.size() <= size)
+			selectedChunks.add(chunk.getWorld().getChunkAt(chunk.getX() - 1, chunk.getZ() - 1));
+		if (!selectedChunks.contains(chunk.getWorld().getChunkAt(chunk.getX() - 1, chunk.getZ() + 1))
+				&& Math.rint(Math.random()) == 1 && selectedChunks.size() <= size)
+			selectedChunks.add(chunk.getWorld().getChunkAt(chunk.getX() - 1, chunk.getZ() + 1));
 		return selectedChunks;
 	}
 }
