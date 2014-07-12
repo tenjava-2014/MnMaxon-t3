e48844fd67b1a508b7b8bb78fd9a4fa54be26a3f
MnMaxon <masontg777@aol.com>
2014-07-12 16:23:57 -0400

    I'm giving up.  It was fun, but I have to do real life things soon and I won't have enough time to finish.

--- src/main/java/com/tenjava/entries/MnMaxon/Village/Road.java
+++ src/main/java/com/tenjava/entries/MnMaxon/Village/Road.java
@@ -15,38 +15,27 @@ public class Road extends VillageChunk {
 		if (north != null)
 			for (int x = 7; x <= 8; x++)
 				for (int z = 16; z >= 8; z--) {
-					Bukkit.broadcastMessage(""
-							+ chunk.getWorld()
+					Boolean good = false;
+					while (!good) {
+						Material blockType = chunk.getWorld()
+								.getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
+								.getRelative(0, 0, 0).getType();
+						if (blockType.equals(Material.LOG_2) || blockType.equals(Material.LEAVES)
+								|| blockType.equals(Material.LEAVES_2) || blockType.equals(Material.LOG)
+								|| blockType.equals(Material.LONG_GRASS) || blockType.equals(Material.GRAVEL))
+							chunk.getWorld()
 									.getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
-									.getType().toString());
+									.getRelative(0, 0, 0).breakNaturally();
+						else {
+							good = true;
+						}
+					}
 					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
 							.getRelative(0, -1, 0).setType(Material.COBBLESTONE);
 					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
-							.setType(Material.GRAVEL);
-					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ());
-					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ());
-					Bukkit.broadcastMessage(""
-							+ chunk.getWorld()
-									.getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
-									.getType().toString());
+							.setType(Material.DIAMOND_ORE);
 				}
 		if (east != null)
-			for (int z = 7; z <= 8; z++)
-				for (int x = 16; x >= 8; x--) {
-					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
-							.getRelative(0, -1, 0).setType(Material.COBBLESTONE);
-					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
-							.setType(Material.GRAVEL);
-				}
-		if (south != null)
-			for (int x = 7; x <= 8; x++)
-				for (int z = 0; z <= 8; z++) {
-					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
-							.getRelative(0, -1, 0).setType(Material.COBBLESTONE);
-					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
-							.setType(Material.GRAVEL);
-				}
-		if (west != null)
 			for (int z = 7; z <= 8; z++)
 				for (int x = 16; x >= 8; x--) {
 					Boolean good = false;
@@ -56,18 +45,61 @@ public class Road extends VillageChunk {
 								.getRelative(0, 0, 0).getType();
 						if (blockType.equals(Material.LOG_2) || blockType.equals(Material.LEAVES)
 								|| blockType.equals(Material.LEAVES_2) || blockType.equals(Material.LOG)
-								|| blockType.equals(Material.LONG_GRASS))
+								|| blockType.equals(Material.LONG_GRASS) || blockType.equals(Material.GRAVEL))
 							chunk.getWorld()
 									.getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
-									.getRelative(0, 0, 0).setType(Material.AIR);
+									.getRelative(0, 0, 0).breakNaturally();
 						else
 							good = true;
 					}
 					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
 							.getRelative(0, -1, 0).setType(Material.COBBLESTONE);
 					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
-							.setType(Material.GRAVEL);
+							.setType(Material.GOLD_ORE);
 				}
-		Bukkit.broadcastMessage("" + chunk.getWorld().getHighestBlockAt(16, 16).getType().toString());
+		if (south != null)
+			for (int x = 7; x <= 8; x++)
+				for (int z = 0; z <= 8; z++) {
+					Boolean good = false;
+					while (!good) {
+						Material blockType = chunk.getWorld()
+								.getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
+								.getRelative(0, 0, 0).getType();
+						if (blockType.equals(Material.LOG_2) || blockType.equals(Material.LEAVES)
+								|| blockType.equals(Material.LEAVES_2) || blockType.equals(Material.LOG)
+								|| blockType.equals(Material.LONG_GRASS) || blockType.equals(Material.GRAVEL))
+							chunk.getWorld()
+									.getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
+									.getRelative(0, 0, 0).breakNaturally();
+						else
+							good = true;
+					}
+					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
+							.getRelative(0, -1, 0).setType(Material.COBBLESTONE);
+					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
+							.setType(Material.DIAMOND_BLOCK);
+				}
+		if (west != null)
+			for (int z = 7; z <= 8; z++)
+				for (int x = 0; x <= 8; x++) {
+					Boolean good = false;
+					while (!good) {
+						Material blockType = chunk.getWorld()
+								.getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
+								.getRelative(0, 0, 0).getType();
+						if (blockType.equals(Material.LOG_2) || blockType.equals(Material.LEAVES)
+								|| blockType.equals(Material.LEAVES_2) || blockType.equals(Material.LOG)
+								|| blockType.equals(Material.LONG_GRASS) || blockType.equals(Material.GRAVEL))
+							chunk.getWorld()
+									.getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
+									.getRelative(0, 0, 0).breakNaturally();
+						else
+							good = true;
+					}
+					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
+							.getRelative(0, -1, 0).setType(Material.COBBLESTONE);
+					chunk.getWorld().getHighestBlockAt(chunk.getBlock(x, 0, z).getX(), chunk.getBlock(x, 0, z).getZ())
+							.setType(Material.GOLD_BLOCK);
+				}
 	}
 }
